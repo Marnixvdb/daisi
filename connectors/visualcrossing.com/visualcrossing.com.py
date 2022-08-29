1e712142be1cf37ade0e6a01ac1db93bd7e9d4c2
Marnix van den Broek <marnix.van.den.broek@bundlesandbatches.io>
2022-08-29 22:08:39 +0200

    hello world

--- connectors/visualcrossing.com/visualcrossing.com.py
+++ connectors/visualcrossing.com/visualcrossing.com.py
@@ -1,45 +1,50 @@
 
-from datetime import datetime
-from pydantic import BaseModel
-from uuid import uuid4
-import requests
-import os
-from typing import Optional
 
-CONNECTOR_NAME = 'visualcrossing.com'
+def hello_world(who="someone"):
+    return f"Hello, {who}!"
 
 
-class ConfigSpec(BaseModel):
-    location: str
-    date1: datetime
-    date2: Optional[datetime] = None
-    key: str
+# from datetime import datetime
+# from pydantic import BaseModel
+# from uuid import uuid4
+# import requests
+# import os
+# from typing import Optional
+
+# CONNECTOR_NAME = 'visualcrossing.com'
 
 
-def config_spec():
-    return ConfigSpec.schema_json()
+# class ConfigSpec(BaseModel):
+#     location: str
+#     date1: datetime
+#     date2: Optional[datetime] = None
+#     key: str
 
 
-def extract(**kwargs):
-    import pyarrow.csv
-    from io import BytesIO
-
-    config = ConfigSpec(**kwargs)
-
-    id = uuid4()
-
-    key = os.environ["VISUAL_CROSSING_API"]
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{config.location}/{config.date1.date().isoformat()}/?unitGroup=metric&include=days&key={config.key}&contentType=csv"
-
-    res = requests.get(url)
-    res.raise_for_status()
-
-    table = pyarrow.csv.read_csv(BytesIO(res.content))
-
-    return table
+# def config_spec():
+#     return ConfigSpec.schema_json()
 
 
-def inspect(table):
-    res = f"table loaded with {table.num_columns} columns and {table.num_rows} rows and {table.nbytes} bytes"
+# def extract(**kwargs):
+#     import pyarrow.csv
+#     from io import BytesIO
 
-    return res
+#     config = ConfigSpec(**kwargs)
+
+#     id = uuid4()
+
+#     key = os.environ["VISUAL_CROSSING_API"]
+#     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{config.location}/{config.date1.date().isoformat()}/?unitGroup=metric&include=days&key={config.key}&contentType=csv"
+
+#     res = requests.get(url)
+#     res.raise_for_status()
+
+#     table = pyarrow.csv.read_csv(BytesIO(res.content))
+
+#     return table
+
+
+# def inspect(table):
+#     res = f"table loaded with {table.num_columns} columns and {table.num_rows} rows and {table.nbytes} bytes"
+
+#     return res
